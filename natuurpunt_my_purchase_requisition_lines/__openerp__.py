de32469caec2a050744821ea79487fe92b8da506
mattiasvo <mattias.vanopstal@natuurpunt.be>
2016-08-18 12:07:05 +0200

    [IMP] #1597 Add menu: My Purchase Requisition Lines

---
+++ natuurpunt_my_purchase_requisition_lines/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name" : "natuurpunt_my_purchase_requisition_lines",
+    "version" : "1.0",
+    "author" : "Natuurpunt (mattias.vanopstal@natuurpunt.be)",
+    "website" : "www.natuurpunt.be",
+    "category" : "Purchase Management",
+    "description": """
+    module for personal purchase requisition lines
+""",
+    "depends" : ["natuurpunt_purchase",
+                 "natuurpunt_purchase_requisition",
+                 "natuurpunt_purchase_search"],
+    "data" : ["natuurpunt_my_purchase_requisition_lines_view.xml"],
+    "init_xml" : [],
+    "update_xml" : [],
+    "active": False,
+    "installable": True
+}
